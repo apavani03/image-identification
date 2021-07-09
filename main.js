6de4b410cd37e167069fb3af03db23b4691a24e4
apavani03 <76710296+apavani03@users.noreply.github.com>
2021-07-09 11:06:30 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+Webcam.attach( '#camera' );
+
+camera = document.getElementById("camera");
+      
+  Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+  });
